9701953fa47e973474d0d86bc8b1ee69198bfa44
Arijit <arijitchandra45@gmail.com>
2024-02-07 01:20:38 +0530

    Added user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose from "mongoose";
+import jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt"; //used to encrypt password
+
+//direct ecryption is not possible so we take use of some hooks of mongoose, such a hook is pre hook
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String //cloudinary url
+        },
+        watchHistory: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+//using the pre hook to encrypt the password just before data is saved. Cant use callback func since there is 
+//no reference of this inside callback
+//the function is written as async because encryption is a time taking process
+userSchema.pre("save", async function(next) {
+    //passing password, rounds
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+});
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    //validating if password entered by user and excrypted password are same
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
+
+
+export const User = mongoose.model('User', userSchema);
